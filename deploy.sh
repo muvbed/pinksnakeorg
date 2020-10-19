84f39b6982d16b0e8511f249274b9196a7f04611
muvbed <muvbed@gmail.com>
2020-10-19 17:16:59 +0300

    changed push path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:muvbed/itprojects-test.git master:gh-pages
+git push -f git@github.com:muvbed/pinksnakeorg.git master:gh-pages
 
 cd -
